de6bb38a922b98090d89ac95645b56a86d6dc267
blacchoy@gmail.com <blacchoy@gmail.com>
2020-06-29 06:34:18 -0700

    create failing 2nd test - mercury age

--- __tests__/supa.test.js
+++ __tests__/supa.test.js
@@ -5,4 +5,8 @@ describe ('User', () => {
   test ('this should test that the users age is 29', () => {
     expect(newUser.age).toEqual(29);
   });
+
+test ('this should test Mercury age calculation', () => {
+  expect(newUser.mercuryAge()).toEqual(120);
+  });
 });
